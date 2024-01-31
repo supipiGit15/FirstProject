a4e3921c54fb8ba1cd4de46b60959084d63f5cc1
supipiGit15 <142130775+supipiGit15@users.noreply.github.com>
2024-01-31 23:03:18 +0530

    Adding 1st code change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'FirstProject';
+  title = 'Welcome to Developers Hub';
 }
